b4e39d67ed95100adf62867428809a9482f37c8a
Cristhian <cristhianfernandolp@gmail.com>
2025-06-20 23:59:45 -0300

    chore: delete unused file

--- server/lib/utils/training.ts
+++
@@ -1,8 +0,0 @@
-import type { TrainingZone } from "../types";
-
-export function getTrainingZone(
-  zones: TrainingZone[],
-  percentage: number,
-): TrainingZone | undefined {
-  return zones.find((zone) => zone.vo2Percentage === percentage);
-}
