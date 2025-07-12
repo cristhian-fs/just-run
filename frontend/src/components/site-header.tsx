54fcb6b3a8794671481da9731c6483f7380b8cc6
Cristhian <cristhianfernandolp@gmail.com>
2025-07-12 14:08:16 -0300

    change location based on pathname

--- frontend/src/components/site-header.tsx
+++ frontend/src/components/site-header.tsx
@@ -1,10 +1,18 @@
+import { useLocation } from "@tanstack/react-router";
+
 import { ModeToggle } from "./mode-toggle";
 
 export function SiteHeader() {
+  const location = useLocation();
+  const locationName =
+    location.pathname.split("/")[1].split("-").join(" ") || "Home";
+  const locationCamelCased =
+    locationName.charAt(0).toUpperCase() + locationName.slice(1);
+
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) bg-sidebar flex shrink-0 items-center gap-2 rounded-md border px-2 py-2 transition-[width,height] ease-linear">
-      <div className="flex w-full items-center gap-1 px-2 lg:gap-2 lg:px-4">
-        <h1 className="text-base font-medium">Dashboard</h1>
+      <div className="flex h-full w-full items-center gap-0 px-2 lg:gap-2">
+        <h1 className="text-base font-medium">{locationCamelCased}</h1>
         <div className="ml-auto flex items-center gap-2">
           <ModeToggle />
         </div>
