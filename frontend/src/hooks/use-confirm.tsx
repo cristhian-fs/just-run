2935a204a2ae926431f82895b0e570edc65b5795
Cristhian <cristhianfernandolp@gmail.com>
2025-07-23 12:28:15 -0300

    feat: add useConfirm hook to handle confirm actions

---
+++ frontend/src/hooks/use-confirm.tsx
@@ -0,0 +1,98 @@
+import { JSX, useEffect, useState } from "react";
+
+import { VariantProps } from "class-variance-authority";
+import { AnimatePresence, motion } from "motion/react";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+
+export const useConfirm = ({
+  title,
+  message,
+  variant,
+  buttonCopy,
+}: {
+  title: string;
+  message: string;
+  variant: VariantProps<typeof buttonVariants>["variant"];
+  buttonCopy: {
+    idle: string;
+    loading: JSX.Element;
+  };
+}): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+  const [buttonState, setButtonState] = useState<"idle" | "loading">("idle");
+
+  useEffect(() => {
+    if (promise !== null) {
+      setButtonState("idle");
+    }
+  }, [promise]);
+
+  const confirm = () => {
+    return new Promise((resolve) => {
+      setPromise({ resolve });
+    });
+  };
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    setButtonState("loading");
+    handleClose();
+  };
+
+  const handleCancel = () => {
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  const ConfirmationDialog = () => (
+    <ResponsiveDialog
+      openDialog={promise !== null}
+      setOpenDialog={handleClose}
+      onEscapeKeyDown={handleCancel}
+      dialogContentClassName="p-0 gap-0"
+      content={
+        <>
+          <DialogHeader className="p-6">
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{message}</DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="border-border bg-secondary/25 dark:bg-secondary/50 justify-end border-t px-6 pb-4 pt-4">
+            <Button
+              className="min-w-60 overflow-hidden"
+              variant={variant}
+              onClick={handleConfirm}
+            >
+              <AnimatePresence mode="popLayout" initial={false}>
+                <motion.span
+                  transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+                  initial={{ opacity: 0, y: -25 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 25 }}
+                  key={buttonState}
+                >
+                  {buttonCopy[buttonState]}
+                </motion.span>
+              </AnimatePresence>
+            </Button>
+          </DialogFooter>
+        </>
+      }
+    />
+  );
+
+  return [ConfirmationDialog, confirm];
+};
