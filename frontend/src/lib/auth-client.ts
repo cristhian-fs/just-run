9a3d2a286e06fb7109870f001ef48e88494382e5
Cristhian <cristhianfernandolp@gmail.com>
2025-08-07 20:46:27 -0300

    chore: change env parameter to new url

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -5,7 +5,7 @@ export const authClient = createAuthClient({
   baseURL:
     import.meta.env.NODE_ENV === "production"
       ? import.meta.env.VITE_APP_URL
-      : import.meta.env.VITE_SERVER_URL,
+      : import.meta.env.AUTH_BASE_URL,
   plugins: [
     inferAdditionalFields({
       user: {
