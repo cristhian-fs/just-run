b62e9cd25a5590966414ffc6a0dac07a8ab60d14
Cristhian <cristhianfernandolp@gmail.com>
2025-08-07 20:25:45 -0300

    feat: change onboarding schemas to fit new onboarding implementation

--- shared/schemas/onboarding.schemas.ts
+++ shared/schemas/onboarding.schemas.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { testTypes, trainingGoals } from "../constants/training.constants";
+import { TestFormSchema } from "./training.schemas";
 
 export const onboardingSchema = z.object({
   name: z.string(),
@@ -26,3 +27,25 @@ export const onboardingSchema = z.object({
   trainingLevel: z.enum(["beginner", "intermediate", "advanced"]),
   goal: z.enum(trainingGoals),
 });
+
+export const onboardingStep1 = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  gender: z.enum(["male", "female", "other"]),
+  age: z.coerce.number().min(0).max(120),
+  weightKg: z.coerce
+    .number()
+    .min(0, { message: "O peso é obrigatório" })
+    .max(300),
+  heightCm: z.coerce
+    .number()
+    .min(0, { message: "A altura é obrigatória" })
+    .max(300),
+  trainingLevel: z.enum(["beginner", "intermediate", "advanced"], {
+    message: "Selecione um nível",
+  }),
+})
+
+export const CombinedOnboardingSchema = onboardingStep1.merge(TestFormSchema);
+
+export type CombinedOnboardingType = z.infer<typeof CombinedOnboardingSchema>;
