e5c844d4b1feeee0979aa31aa5e1ab461189ba91
cristhian <cristhianfernandolp@gmail.com>
2025-10-23 17:07:54 -0300

    chore: update files to deployment

--- server/index.ts
+++ server/index.ts
@@ -10,24 +10,24 @@ const app = createApp();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
-	.basePath("/api")
-	.route("/users", userRouter)
-	.route("/trainings", trainingRouter)
-	.route("/analytics", analyticsRouter)
-	.route("/running-tests", runningTestsRouter)
-	.route("/plans", plansRouter);
+  .basePath("/api")
+  .route("/users", userRouter)
+  .route("/trainings", trainingRouter)
+  .route("/analytics", analyticsRouter)
+  .route("/running-tests", runningTestsRouter)
+  .route("/plans", plansRouter);
 
-app.get("*", serveStatic({ root: "./frontend/dist/" }));
+app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 // IMPORTANT: Development mode, export the app
-export default app;
+// export default app;
 
 // IMPORTANT: Production mode, export the below
-// export default {
-//   port: process.env.PORT || 3000,
-//   hostname: "0.0.0.0",
-//   fetch: app.fetch,
-// };
+export default {
+  port: process.env.PORT || 3000,
+  hostname: "0.0.0.0",
+  fetch: app.fetch,
+};
 
 export type ApiRoutes = typeof routes;
